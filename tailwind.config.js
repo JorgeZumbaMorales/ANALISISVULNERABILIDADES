af9c21a919c26c2089d7f34951f209ada48418a9
JorgeZumbaMorales <jorgezumbamorales2000@hotmail.com>
2025-02-23 01:31:09 -0500

    Diseño

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,17 @@
 module.exports = {
   content: [
     "./src/**/*.{html,ts}",
+
   ],
   theme: {
     extend: {
       colors: {
-        primary: "#0A2342", // Azul oscuro
-        secondary: "#3E92CC", // Azul claro
-        accent: "#ff3f00", // Naranja
-        background: "#F4F4F9", // Blanco suave
-        textPrimary: "#4A4A4A", // Gris oscuro
-        textSecondary: "#FFFFFF", // Blanco
+        baseAzulOscuro: "#0A2342", // Azul oscuro
+        baseAzulClaro: "#3E92CC", // Azul claro
+        baseNaranja: "#ff3f00", // Naranja
+        fondoClaro: "#F4F4F9", // Blanco suave
+        textoPrincipal: "#4A4A4A", // Gris oscuro
+        textoSecundario: "#FFFFFF", // Blanco
       },
     },
   },
